901ae534991433b86de437b1654d9348358ae912
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-06-03 20:58:44 -0400

    basic rps functionality

--- src/apps/rps/App.jsx
+++ src/apps/rps/App.jsx
@@ -4,48 +4,64 @@ import paper from './assets/images/paper.jpeg'
 import scissors from './assets/images/scissors.jpg'
 import {useState} from "react";
 
+
 const App = () => {
 
 	const [player, setPlayer] = useState('')
 	const [cpu, setCpu] = useState('')
+	const [score, setScore] = useState({player: 0, cpu: 0})
+	const [userAction, setUserAction] = useState(true)
 
 	const options = [rock, paper, scissors]
 
-
-	const runCPU = () => {
-		console.log('CPU thinking...')
-		const rand = Math.floor(Math.random() * 2)
-		setTimeout(() => {}, 3000)
-		setCpu(prev => options[rand])
-		console.log(cpu)
+	const calcScore = () => {
+		if (player === cpu) {
+			return
+		}
+		if ((player === 'rock' && cpu === 'scissors') || (player === 'paper' && cpu === 'rock') || (player === 'scissors' && cpu === 'paper')) {
+			setScore({...score, player: score.player + 1})
+		}
+		else {
+			setScore({...score, cpu: score.cpu + 1})
+		}
 	}
 
 	const handleClick = (option) => {
+		setUserAction(false)
 		setPlayer(option)
-		console.log(player)
-		runCPU()
+		setCpu(options[Math.floor(Math.random() * 2)])
+		calcScore()
 	}
-
 
 	return (
 		<>
 			<div>
 				<h2>Rock, Paper, Scissors</h2>
 			</div>
+			<div className='score'>
+				Player {score.player} :
+				: {score.cpu} CPU
+			</div>
 			<div className='game'>
-				<div className='player'>Player
+				<div className='player'>
 					<img src={player} alt='p1-image'/>
 				</div>
 
-				<div className='cpu'>CPU
+				<div className='cpu'>
 					<img src={cpu} alt='cpu-image'/>
 				</div>
 			</div>
 
-
-			<button onClick={()=>handleClick(rock)}>Rock</button>
-			<button onClick={()=>handleClick(paper)}>Paper</button>
-			<button onClick={()=>handleClick(scissors)}>Scissors</button>
+			{userAction ?
+			<>
+				<button onClick={()=>handleClick(rock)}>Rock</button>
+				<button onClick={()=>handleClick(paper)}>Paper</button>
+				<button onClick={()=>handleClick(scissors)}>Scissors</button>
+			</>
+				:
+			<>
+				<button onClick={()=>setUserAction(!userAction)}>Play again!</button>
+			</>}
 		</>
 	)
 
